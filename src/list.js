caabf7388fb7a5aad35bccf9717584bbaa194b38
FukcThat <63652e636f6465@gmail.com>
2024-12-11 21:59:54 +0100

    Add list creation modal

---
+++ src/list.js
@@ -0,0 +1,7 @@
+const addListBtn = document.querySelector(".list-option--make-list");
+const customLists = document.querySelector(".list-option--custom-lists");
+
+// onclick on add list btn to open add-llist-modal
+// get input value
+// onclick on done add value to array of lists
+// display new list in list container
