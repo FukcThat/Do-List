8c305b03b1d3b62e769b0f38bf9d3dad29ae9f18
FukcThat <63652e636f6465@gmail.com>
2024-12-12 23:13:56 +0100

    ADD: Add-new-List modal

--- src/list.js
+++ src/list.js
@@ -1,7 +1,30 @@
-const addListBtn = document.querySelector(".list-option--make-list");
+const makeListBtn = document.querySelector(".list-option--make-list");
 const customLists = document.querySelector(".list-option--custom-lists");
+const listModal = document.querySelector(".list--modal");
+const newListInput = document.querySelector(".modal--input");
+const addListBtn = document.querySelector(".modal-add-list-btn");
 
-// onclick on add list btn to open add-llist-modal
-// get input value
-// onclick on done add value to array of lists
-// display new list in list container
+let listArray = [];
+
+// onclick on add list btn to open add-list-modal
+makeListBtn.addEventListener("click", () => {
+  listModal.classList.remove("hidden");
+});
+
+addListBtn.addEventListener("click", () => {
+  const newListName = newListInput.value;
+  listArray.push(newListName);
+
+  renderLists();
+});
+
+// Helper to render list in ul in nav
+const renderLists = (array) => {
+  customLists.innerHTML = "";
+
+  array.forEach((list) => {
+    const listBtn = document.createElement("button");
+    listBtn.textContent = list;
+    customLists.appendChild(listBtn);
+  });
+};
