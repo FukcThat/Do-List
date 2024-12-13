c233cd600ad2bc3a01cff72be3fde3506ddf2b3f
FukcThat <63652e636f6465@gmail.com>
2024-12-13 23:36:15 +0100

    FIX: List Modal actually works now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./styles.css";
+
+import listHandler from "./list";
+
+listHandler();
--- src/list.js
+++ src/list.js
@@ -1,30 +1,44 @@
-const makeListBtn = document.querySelector(".list-option--make-list");
-const customLists = document.querySelector(".list-option--custom-lists");
-const listModal = document.querySelector(".list--modal");
-const newListInput = document.querySelector(".modal--input");
-const addListBtn = document.querySelector(".modal-add-list-btn");
+const listHandler = () => {
+  const toggleListModalBtn = document.querySelector(".list--open-modal-btn");
+  const customLists = document.querySelector(".list-option--custom-lists");
+  const listModal = document.querySelector(".list--modal");
+  const newListInput = document.querySelector(".modal--input");
+  const addListBtn = document.querySelector(".modal-add-list-btn");
 
-let listArray = [];
+  let listArray = [];
 
-// onclick on add list btn to open add-list-modal
-makeListBtn.addEventListener("click", () => {
-  listModal.classList.remove("hidden");
-});
-
-addListBtn.addEventListener("click", () => {
-  const newListName = newListInput.value;
-  listArray.push(newListName);
-
-  renderLists();
-});
-
-// Helper to render list in ul in nav
-const renderLists = (array) => {
-  customLists.innerHTML = "";
-
-  array.forEach((list) => {
-    const listBtn = document.createElement("button");
-    listBtn.textContent = list;
-    customLists.appendChild(listBtn);
+  // Toggle List creation modal
+  toggleListModalBtn.addEventListener("click", () => {
+    if (listModal.classList.contains("hidden")) {
+      listModal.classList.remove("hidden");
+      listModal.classList.add("flex");
+    } else {
+      listModal.classList.add("hidden");
+      listModal.classList.remove("flex");
+    }
   });
+
+  // Submit new list
+  addListBtn.addEventListener("click", () => {
+    const newListName = newListInput.value;
+    listArray.push(newListName);
+
+    newListInput.value = "";
+
+    renderLists(listArray);
+  });
+
+  // Render Lists
+  const renderLists = (array) => {
+    customLists.innerHTML = "";
+
+    array.forEach((list) => {
+      const listBtn = document.createElement("div");
+      listBtn.textContent = list;
+      listBtn.classList.add("list-modal--list-btn", "nav-btn");
+      customLists.appendChild(listBtn);
+    });
+  };
 };
+
+export default listHandler;
